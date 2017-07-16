43f93a22913b5a3db175f3614be7d0f1b177791f
Sam Wu <sjwu@ualberta.ca>
2017-07-15 23:30:45 -0700

    doc symbols

--- Connect4/src/game/Symbols.java
+++ Connect4/src/game/Symbols.java
@@ -3,8 +3,11 @@ package game;
 /**
  * 
  * Enumerator for Connect Four game chips/symbols
+ * Symbols is an Enum Type with the following predefined constants:
+ * Red, Yellow, Space
  *
  */
+//https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
 public enum Symbols {
 	//symbol codes at http://www.alt-codes.net/
 	
@@ -21,13 +24,19 @@ public enum Symbols {
 		this.chip = chip;
 	}
 	
-	//convert Symbols to char
+	/**
+	 * convert Symbols (Enum Type) to char
+	 * @return chip as char data type
+	 */
 	public char toChar() {
         return this.chip;
     }
 	
-	@Override
-    //convert Symbols to string
+	/**
+     * @Override
+     * convert Symbols (Enum Type) to string
+     * @return chip as String data type
+     */
     public String toString() {
         return String.format("%c", this.chip);
     }
